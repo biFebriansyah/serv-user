1c840d533e53bef132278d50557c093e20a187e7
ebifebriansyah <bukanebi@gmail.com>
2020-05-06 16:02:27 +0700

    update host ansibel

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ const Users = require("./Routes/Users");
 const MainRouters = express.Router();
 
 MainRouters.use("/users", Users);
+MainRouters.get("*", function (req, res) {
+    res.status(404).send("what???");
+});
 
 module.exports = MainRouters;
